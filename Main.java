504ce2ddf78584d73e25d3d677d7fbcf4849cf71
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-04-04 22:42:13 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,17 +19,4 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-
-        class Cliente{
-            String nome;
-            long tel;
-            float saldo;
-        }
-        class pedido{
-            
-        }
-
-        private void registrarCliente()
-
-    }
 }
