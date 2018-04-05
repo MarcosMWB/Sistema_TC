83c577e01e0af186ed8e606a96cae22200eb2668
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-04-05 01:14:11 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,27 @@
-package sample;
+package temperocerto.sistema_tc;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Main extends Application {
+public class Main extends AppCompatActivity {
+
+    float small = 10;
+    float big = 12;
+    float total = 0;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Tempero Certo");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temperocerto_main);
     }
 
+    public void addProfit(byte size, float small, float big, float total){
+        if(size == 1){ //count small size order
+            this.total += small;
+        }
+        if(size == 2){ //count big size order
+            this.total += big;
+        }
+    }
 
-    public static void main(String[] args) {
-        launch(args);
 }
