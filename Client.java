710108a323afd870240ec61d3c9e88b86b9d0ca6
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-04-05 01:12:55 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,5 +1,15 @@
-public class Client{
-    String nome;
-    int tel;
-    SaldoMensal saldo;
+package temperocerto.sistema_tc;
+
+public class Client {
+    String name;
+    int phone;
+    float balance;
+    Order order;
+
+    public void addClient(String name, int phone, float balance, Order order){
+        this.name = name;
+        this.phone = phone;
+        this.balance = balance;
+        this.order = order;
+    }
 }
